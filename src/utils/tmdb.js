8a9d7d253e16055ef96d872b675d3a41e6f50c67
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 15:13:30 +0530

    Generating Images using Image Api URL

---
+++ src/utils/tmdb.js
@@ -0,0 +1,17 @@
+import path from 'path';
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+export const ImageSizes = {
+  poster: 'w500',
+  card: 'w300',
+  backdrop: 'w1280',
+};
+
+export function generateImageUrl(imagePath, size) {
+  return path.join(IMAGE_BASE_URL, size, imagePath);
+}
+
+export function filterPreferredResults(results) {
+  return results.filter((o) => ['movie', 'tv'].includes(o.media_type));
+}
