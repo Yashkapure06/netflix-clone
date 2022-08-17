faf26e05293810c3fdeebe3579e49b96e0b3a15f
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 22:25:25 +0530

    Added slider for the the movie  cards scroll along y-axis

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const Slider = () => {
+import tmdbApi from '../api/tmdb';
+import Cards from "./Cards";
+
+const Slider = ({ mediaType, title, path, params = {}, isLarge }) => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+        const json = await tmdbApi.get(path, params);
+        setItems(json.results);
+        };
+        fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    
     return (
-        <div>
-            
-        </div>
+        <div className="media-slider">
+      <h3 className="media-slider__title">{title}</h3>
+      <div className="media-slider__cards">
+        {items.map((media) => (
+          <Cards
+            isLarge={isLarge}
+            key={media.id}
+            media={media}
+            mediaType={media.media_type || mediaType}
+          />
+        ))}
+      </div>
+    </div>
     );
 }
 
