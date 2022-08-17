b8ed3083fa19abae8b989f6055dcc2021f2cc7e0
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 12:54:52 +0530

    imported styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import './index.scss';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
