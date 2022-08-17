16629d179a1f09d406a4aa8bcb32e5db898f2edf
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 22:25:42 +0530

    removed small bug

--- src/api/TMDB.js
+++ src/api/TMDB.js
@@ -1,11 +1,12 @@
 import secret from '../secrets.json';
 import Api from '../utils/api';
+
 const API_BASE = 'https://api.themoviedb.org/3';
 const API_KEY =  secret.TMDB_API_KEY;
 
 
 const tmdb =  new Api({
-    base: API_BASE,
+    baseUrl: API_BASE,
     searchParams: { api_key: API_KEY },
 })
 
