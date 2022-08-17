6196a1da25208a5940d7d468bbd29fbc7a8f732e
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 15:14:54 +0530

    Here we generate URL endpoints and parse JSON Responses

---
+++ src/utils/api.js
@@ -0,0 +1,47 @@
+import path from "path";
+import qs from "query-string";
+
+
+class Api {
+    constructor({ baseUrl, searchParams}){
+        this._baseUrl = baseUrl;
+        this._searchParams = searchParams || {};
+    }
+    async get(endpoint, params) {
+        const hitUrl = this._generateUrl(endpoint, params);
+        const response = await fetch(hitUrl, {
+          method: 'GET',
+        });
+    
+        return this._parseJsonResponse(response);
+      }
+    
+      async _parseJsonResponse(response) {
+        let json = null;
+        try {
+          json = await response.json();
+        } catch (e) {
+            throw new Error(`Failed to parse response JSON: ${e.message}`);
+        }
+    
+        if (response.ok) {
+          return json;
+        } else {
+          const error = new Error(response.statusText);
+          error.isFromServer = true;
+          error.response = response;
+          error.responseJson = json;
+    
+          throw error;
+        }
+      }
+    
+      _generateUrl(endpoint, params) {
+        const search = qs.stringify({ ...this._baseSearchParams, ...params });
+        const url = path.join(this._baseUrl, endpoint);
+        return [url, search].join('?');
+      }
+}
+
+
+export default Api;
