d789a8339e04cedc4c2d125f17138fe9a2333d78
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 21:58:30 +0530

    Fetching Movie name, description and image and display in the form of cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
-const Cards = () => {
+
+import { generateImageUrl, ImageSizes } from "../utils/tmdb";
+import TrailerModal from './TrailerModal';
+
+
+const Cards = ({ media, mediaType, isLarge }) => {
+
+    const [ isTrailerOpen, setTrailerOpen ] = useState(false);
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div
+                key={media.id}
+                className="media-card"
+                onClick={() => setTrailerOpen(true)}
+            >
+                <img
+                className={classNames(
+                    'media-card__poster',
+                    { 'media-card__poster--large': isLarge },
+                )}
+                src={
+                    isLarge
+                    ? generateImageUrl(media.poster_path, ImageSizes.poster)
+                    : generateImageUrl(media.backdrop_path, ImageSizes.card)
+                }
+                alt={media.original_title}
+                />
+                <div className="media-card__cover">
+                    <div className="media-card__name">
+                        {media.title || media.name || media.original_name}
+                    </div>
+                    <div className="media-card__description">
+                        {media.overview}
+                    </div>
+                </div>
+            </div>
+            {isTrailerOpen && (
+                <TrailerModal
+                    mediaType={mediaType}
+                    mediaId={media.id}
+                    media={media}
+                    onClose={() => setTrailerOpen(false)}
+                />
+            )}
+        </>
     );
 }
 
