6569fb23ca0784e46b3898b27e4b03ad814a3ab7
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 15:14:00 +0530

    Main tmdb API

---
+++ src/api/TMDB.js
@@ -0,0 +1,12 @@
+import secret from '../secrets.json';
+import Api from '../utils/api';
+const API_BASE = 'https://api.themoviedb.org/3';
+const API_KEY =  secret.TMDB_API_KEY;
+
+
+const tmdb =  new Api({
+    base: API_BASE,
+    searchParams: { api_key: API_KEY },
+})
+
+export default tmdb;
