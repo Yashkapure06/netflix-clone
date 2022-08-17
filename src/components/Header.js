b335b645d5e38d49b66633248747ff6e2bc0be5c
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-17 12:55:25 +0530

    Update in header - still working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+
+
+
+import logo from '../assets/logo-full.png';
+
 
 const Header = () => {
+
+    const [ floating, setFloating] = useState(false);
+
+    useEffect(()=>{
+        function handleScroll() {
+            if( window.scrollY > 100 ) {
+                setFloating(true);
+            } else {
+                setFloating(false);
+            }
+        }
+
+        window.addEventListener("scroll",handleScroll);
+        return () => {
+            window.removeEventListener("scroll",handleScroll);
+        }
+
+    },[])
+
+
+
     return (
-        <div>
-            
+        <div
+            className={classNames(
+                "header",
+                { 'header--float': floating }
+            )}
+            >
+            <img
+                className="header__logo"
+                src={logo}
+                alt="Netflix logo"
+            />
+            <img
+                className="header__avatar"
+                // src={avatar}
+                alt="Netflix avatar"
+            />
         </div>
     );
 }
